2ca23a0246dad7b6d82f353cb393a279846b5a98
Austeja Kalpakovaite <austeja.kalpakovaite@bentley.com>
2021-09-14 12:45:58 +0300

    WIP

--- tests/imodels-clients-tests/src/Auth.test.ts
+++ tests/imodels-clients-tests/src/Auth.test.ts
@@ -20,77 +20,65 @@ interface TestUserCredentials {
 }
 
 export class TestAuthClient {
-  public async getAccessToken(params: {
-    authConfig: AuthConfig,
-    testUserCredentials: TestUserCredentials
-  }): Promise<string> {
+  constructor(private _authConfig: AuthConfig) {
+  }
+
+  public async getAccessToken(testUserCredentials: TestUserCredentials): Promise<string> {
     const browserLaunchOptions: puppeteer.LaunchOptions & puppeteer.BrowserLaunchArgumentOptions = { dumpio: true, headless: true };
     const browser: puppeteer.Browser = await puppeteer.launch(browserLaunchOptions);
     const browserPage: puppeteer.Page = await browser.newPage();
 
-    await browserPage.setRequestInterception(true);
-    const authorizationCodePromise = this.interceptRedirectAndGetAuthorizationCode({ browserPage, ...params });
+    const authorizationCodePromise = this.interceptRedirectAndGetAuthorizationCode(browserPage);
 
-    await browserPage.goto(this.getAuthenticationUrl(params), { waitUntil: "networkidle2" });
-    await this.fillCredentials({ browserPage, ...params });
-    const accessToken = await this.exchangeAuthorizationCodeForAccessToken({ authorizationCode: await authorizationCodePromise, ...params });
+    await browserPage.goto(this.getAuthenticationUrl(), { waitUntil: "networkidle2" });
+    await this.fillCredentials(browserPage, testUserCredentials);
+    const accessToken = await this.exchangeAuthorizationCodeForAccessToken(await authorizationCodePromise);
 
     await browser.close();
     return accessToken;
   }
 
-  private getAuthenticationUrl(params: {
-    authConfig: AuthConfig
-  }): string {
-    return `${params.authConfig.authority}/connect/authorize?` +
-      `client_id=${encodeURIComponent(params.authConfig.clientId)}&` +
-      `scope=${encodeURIComponent(params.authConfig.scopes)}&` +
+  private getAuthenticationUrl(): string {
+    return `${this._authConfig.authority}/connect/authorize?` +
+      `client_id=${encodeURIComponent(this._authConfig.clientId)}&` +
+      `scope=${encodeURIComponent(this._authConfig.scopes)}&` +
       `response_type=code&` +
-      `redirect_uri=${encodeURIComponent(params.authConfig.redirectUrl)}`;
+      `redirect_uri=${encodeURIComponent(this._authConfig.redirectUrl)}`;
   }
 
-  private async fillCredentials(params: {
-    browserPage: puppeteer.Page,
-    testUserCredentials: TestUserCredentials
-  }): Promise<void> {
-    const emailField = await params.browserPage.waitForSelector("#identifierInput");
-    await emailField.type(params.testUserCredentials.email);
+  private async fillCredentials(browserPage: puppeteer.Page, testUserCredentials: TestUserCredentials): Promise<void> {
+    const emailField = await browserPage.waitForSelector("#identifierInput");
+    await emailField.type(testUserCredentials.email);
 
-    const nextButton = await params.browserPage.waitForSelector("#sign-in-button");
+    const nextButton = await browserPage.waitForSelector("#sign-in-button");
     await nextButton.click();
 
-    const passwordField = await params.browserPage.waitForSelector("#password");
-    await passwordField.type(params.testUserCredentials.password);
+    const passwordField = await browserPage.waitForSelector("#password");
+    await passwordField.type(testUserCredentials.password);
 
-    const signInButton = await params.browserPage.waitForSelector("#sign-in-button");
+    const signInButton = await browserPage.waitForSelector("#sign-in-button");
     await Promise.all([
       signInButton.click(),
-      params.browserPage.waitForNavigation({ waitUntil: "networkidle2" })
+      browserPage.waitForNavigation({ waitUntil: "networkidle2" })
     ]);
 
-    await this.handleConsentPageIfNeeded(params);
+    await this.handleConsentPageIfNeeded(browserPage);
   }
 
-  private async handleConsentPageIfNeeded(params: {
-    browserPage: puppeteer.Page
-  }): Promise<void> {
-    const isConsentPage = params.browserPage.url().indexOf("consent") !== -1;
+  private async handleConsentPageIfNeeded(browserPage: puppeteer.Page): Promise<void> {
+    const isConsentPage = browserPage.url().indexOf("consent") !== -1; // todo double check if logic is correct
     if (!isConsentPage)
       return;
 
-    const consentButton = await params.browserPage.waitForSelector("#bentleySubmit");
+    const consentButton = await browserPage.waitForSelector("#bentleySubmit");
     await Promise.all([
       consentButton.click(),
-      params.browserPage.waitForNavigation({ waitUntil: "networkidle2" })
+      browserPage.waitForNavigation({ waitUntil: "networkidle2" }) // todo: event names into constants
     ]);
   }
 
-  private async exchangeAuthorizationCodeForAccessToken(params: {
-    authorizationCode: string,
-    authConfig: AuthConfig
-  }): Promise<string> {
-    const authorizationCode = this.getCodeFromUrl(params.authorizationCode);
-    const encodedClientCredentials = Buffer.from(`${encodeURIComponent(params.authConfig.clientId)}:${encodeURIComponent(params.authConfig.clientSecret)}`).toString('base64');
+  private async exchangeAuthorizationCodeForAccessToken(authorizationCode: string): Promise<string> {
+    const encodedClientCredentials = Buffer.from(`${encodeURIComponent(this._authConfig.clientId)}:${encodeURIComponent(this._authConfig.clientSecret)}`).toString('base64');
 
     const response: AxiosResponse<AccessTokenResponse> = await axios({
       method: "POST",
@@ -101,9 +89,9 @@ export class TestAuthClient {
       data: new URLSearchParams({
         grant_type: "authorization_code",
         code: authorizationCode,
-        redirect_uri: params.authConfig.redirectUrl
+        redirect_uri: this._authConfig.redirectUrl
       }),
-      url: `${params.authConfig.authority}/connect/token`
+      url: `${this._authConfig.authority}/connect/token`
     });
 
     return response.data.access_token;
@@ -116,11 +104,12 @@ export class TestAuthClient {
     return code;
   }
 
-  private async interceptRedirectAndGetAuthorizationCode(params: { browserPage: puppeteer.Page, authConfig: AuthConfig }): Promise<string> {
+  private async interceptRedirectAndGetAuthorizationCode(browserPage: puppeteer.Page): Promise<string> {
+    await browserPage.setRequestInterception(true);
     return new Promise<string>((resolve) => {
-      params.browserPage.on("request", async (interceptedRequest) => {
+      browserPage.on("request", async (interceptedRequest) => {
         const currentRequestUrl = interceptedRequest.url();
-        if (!currentRequestUrl.startsWith(params.authConfig.redirectUrl)) {
+        if (!currentRequestUrl.startsWith(this._authConfig.redirectUrl)) {
           interceptedRequest.continue();
         } else {
           await interceptedRequest.respond({
