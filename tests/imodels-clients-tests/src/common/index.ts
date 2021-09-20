c28513f1086ae04143df121f23112194e2900f44
Austeja Kalpakovaite <austeja.kalpakovaite@bentley.com>
2021-09-20 16:58:02 +0300

    Added header to index.ts file and grouped/reordered exports

--- tests/imodels-clients-tests/src/common/index.ts
+++ tests/imodels-clients-tests/src/common/index.ts
@@ -1,11 +1,18 @@
-export * from "./AssertionUtils";
-export * from "./CommonTestUtils";
+/*---------------------------------------------------------------------------------------------
+ * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
+ * See LICENSE.md in the project root for license terms and full copyright notice.
+ *--------------------------------------------------------------------------------------------*/
 export * from "./Config";
 export * from "./Constants";
-export * from "./ProjectsClient";
-export * from "./TestAuthenticationClient";
-export * from "./TestAuthenticationprovider";
 export * from "./TestClientOptions";
 export * from "./TestiModelGroup";
 export * from "./TestiModelMetadata";
+
+export * from "./AssertionUtils";
+export * from "./CommonTestUtils";
+
+export * from "./ProjectsClient";
+export * from "./TestAuthenticationClient";
+
 export * from "./TestProjectProvider";
+export * from "./TestAuthenticationProvider";
