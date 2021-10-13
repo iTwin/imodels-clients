9cbe3bb868f26ff68f09f488e761a906715782ab
Austeja Kalpakovaite <austeja.kalpakovaite@bentley.com>
2021-10-13 16:23:01 +0300

    Making use of the new reusable test iModel infrastructure: adding two named versions to the default test iModel, fixing checkpoint operations tests to accomodate that

--- tests/imodels-clients-tests/src/authoring/CheckpointOperations.test.ts
+++ tests/imodels-clients-tests/src/authoring/CheckpointOperations.test.ts
@@ -4,22 +4,16 @@
  *--------------------------------------------------------------------------------------------*/
 import { GetCheckpointByChangesetIdParams, GetCheckpointByChangesetIndexParams, GetCheckpointByNamedVersionIdParams, iModelsClient, RequestContext, iModelsErrorCode, iModelScopedOperationParams, CheckpointState } from "@itwin/imodels-client-authoring";
 import { expect } from "chai";
-import { TestiModelGroup, TestClientOptions, TestAuthenticationProvider, TestProjectProvider, Constants, cleanUpiModels, assertError, Config, TestSetupError, sleep, assertCheckpoint } from "../common";
-import { TestiModelWithChangesets, TestiModelProvider } from "../common/TestiModelProvider";
-
-interface iModelTimelinePoint {
-  changesetId: string;
-  changesetIndex: number;
-  namedVersionId: string;
-}
+import { TestiModelGroup, TestClientOptions, TestAuthenticationProvider, TestProjectProvider, Constants, cleanUpiModels, assertError, Config, assertCheckpoint } from "../common";
+import { TestiModelNamedVersion, TestiModelProvider, TestiModelWithChangesetsAndNamedVersions } from "../common/TestiModelProvider";
 
 describe("[Authoring] CheckpointOperations", () => {
   let imodelsClient: iModelsClient;
   let requestContext: RequestContext;
   let projectId: string;
   let testiModelGroup: TestiModelGroup;
-  let testiModel: TestiModelWithChangesets;
-  let imodelPointWithCheckpoint: iModelTimelinePoint;
+  let testiModel: TestiModelWithChangesetsAndNamedVersions;
+  let testiModelNamedVersion: TestiModelNamedVersion;
 
   before(async () => {
     imodelsClient = new iModelsClient(new TestClientOptions());
@@ -32,17 +26,12 @@ describe("[Authoring] CheckpointOperations", () => {
       }
     });
 
-    testiModel = await TestiModelProvider.createWithChangesets({
+    testiModel = await TestiModelProvider.getOrCreateReusable({
       requestContext,
       imodelsClient,
-      projectId,
-      imodelName: testiModelGroup.getPrefixediModelName("Test iModel for write")
+      projectId
     });
-
-    const changesetIndexWithCheckpoint = 5;
-    const requestContextForUser2 = await TestAuthenticationProvider.getRequestContext(Config.get().testUsers.user2);
-    imodelPointWithCheckpoint = await setupNamedVersion({ requestContext: requestContextForUser2, changesetIndex: changesetIndexWithCheckpoint });
-    await waitForNamedVersionCheckpointGenerated(imodelPointWithCheckpoint.namedVersionId);
+    testiModelNamedVersion = testiModel.namedVersions[0];
   });
 
   after(async () => {
@@ -54,7 +43,7 @@ describe("[Authoring] CheckpointOperations", () => {
     const getCheckpointByChangesetIdParams: GetCheckpointByChangesetIdParams = {
       requestContext,
       imodelId: testiModel.id,
-      changesetId: imodelPointWithCheckpoint.changesetId
+      changesetId: testiModelNamedVersion.changesetId
     };
 
     // Act
@@ -64,8 +53,8 @@ describe("[Authoring] CheckpointOperations", () => {
     assertCheckpoint({
       actualCheckpoint: checkpoint,
       expectedCheckpointProperties: {
-        changesetId: imodelPointWithCheckpoint.changesetId,
-        changesetIndex: imodelPointWithCheckpoint.changesetIndex,
+        changesetId: testiModelNamedVersion.changesetId,
+        changesetIndex: testiModelNamedVersion.changesetIndex,
         state: CheckpointState.Successful
       }
     });
@@ -76,7 +65,7 @@ describe("[Authoring] CheckpointOperations", () => {
     const getCheckpointByChangesetIndexParams: GetCheckpointByChangesetIndexParams = {
       requestContext,
       imodelId: testiModel.id,
-      changesetIndex: imodelPointWithCheckpoint.changesetIndex
+      changesetIndex: testiModelNamedVersion.changesetIndex
     };
 
     // Act
@@ -86,8 +75,8 @@ describe("[Authoring] CheckpointOperations", () => {
     assertCheckpoint({
       actualCheckpoint: checkpoint,
       expectedCheckpointProperties: {
-        changesetId: imodelPointWithCheckpoint.changesetId,
-        changesetIndex: imodelPointWithCheckpoint.changesetIndex,
+        changesetId: testiModelNamedVersion.changesetId,
+        changesetIndex: testiModelNamedVersion.changesetIndex,
         state: CheckpointState.Successful
       }
     });
@@ -98,7 +87,7 @@ describe("[Authoring] CheckpointOperations", () => {
     const getCheckpointByNamedVersionIdParams: GetCheckpointByNamedVersionIdParams = {
       requestContext,
       imodelId: testiModel.id,
-      namedVersionId: imodelPointWithCheckpoint.namedVersionId
+      namedVersionId: testiModelNamedVersion.id
     };
 
     // Act
@@ -108,8 +97,8 @@ describe("[Authoring] CheckpointOperations", () => {
     assertCheckpoint({
       actualCheckpoint: checkpoint,
       expectedCheckpointProperties: {
-        changesetId: imodelPointWithCheckpoint.changesetId,
-        changesetIndex: imodelPointWithCheckpoint.changesetIndex,
+        changesetId: testiModelNamedVersion.changesetId,
+        changesetIndex: testiModelNamedVersion.changesetIndex,
         state: CheckpointState.Successful
       }
     });
@@ -121,7 +110,7 @@ describe("[Authoring] CheckpointOperations", () => {
       functionUnderTest: (params: iModelScopedOperationParams) => imodelsClient.Checkpoints.getByChangesetId(
         {
           ...params,
-          changesetId: imodelPointWithCheckpoint.changesetId
+          changesetId: testiModelNamedVersion.changesetId
         })
     },
     {
@@ -129,7 +118,7 @@ describe("[Authoring] CheckpointOperations", () => {
       functionUnderTest: (params: iModelScopedOperationParams) => imodelsClient.Checkpoints.getByChangesetIndex(
         {
           ...params,
-          changesetIndex: imodelPointWithCheckpoint.changesetIndex
+          changesetIndex: testiModelNamedVersion.changesetIndex
         })
     },
     {
@@ -137,7 +126,7 @@ describe("[Authoring] CheckpointOperations", () => {
       functionUnderTest: (params: iModelScopedOperationParams) => imodelsClient.Checkpoints.getByNamedVersionId(
         {
           ...params,
-          namedVersionId: imodelPointWithCheckpoint.namedVersionId
+          namedVersionId: testiModelNamedVersion.id
         })
     }
   ].forEach(testCase => {
@@ -239,43 +228,4 @@ describe("[Authoring] CheckpointOperations", () => {
       }
     });
   });
-
-  async function setupNamedVersion(params: { requestContext: RequestContext, changesetIndex: number }): Promise<iModelTimelinePoint> { // TODO rename
-    const changesetMetadata = testiModel.changesets[params.changesetIndex - 1];
-    const namedVersion = await imodelsClient.NamedVersions.create({
-      requestContext: params.requestContext,
-      imodelId: testiModel.id,
-      namedVersionProperties: {
-        name: `Named Version ${changesetMetadata.index}`,
-        changesetId: changesetMetadata.id
-      }
-    });
-    return {
-      changesetId: changesetMetadata.id,
-      changesetIndex: changesetMetadata.index,
-      namedVersionId: namedVersion.id
-    }
-  }
-
-  async function waitForNamedVersionCheckpointGenerated(namedVersionId: string): Promise<void> { // TODO: rethink params
-    const sleepPeriodInMs = 1000;
-    const timeOutInMs = 5 * 60 * 1000;
-    for (let retries = timeOutInMs / sleepPeriodInMs; retries > 0; --retries) {
-      const checkpoint = await imodelsClient.Checkpoints.getByNamedVersionId({
-        requestContext,
-        imodelId: testiModel.id,
-        namedVersionId
-      });
-
-      if (checkpoint.state === CheckpointState.Successful)
-        return;
-
-      if (checkpoint.state !== CheckpointState.Scheduled)
-        throw new TestSetupError(`Checkpoint generation failed with state: ${checkpoint.state}.`);
-
-      await sleep(sleepPeriodInMs);
-    }
-
-    throw new TestSetupError(`Timed out while waiting for checkpoint generation to complete.`);
-  }
 });
--- tests/imodels-clients-tests/src/common/TestiModelProvider.ts
+++ tests/imodels-clients-tests/src/common/TestiModelProvider.ts
@@ -1,6 +1,6 @@
-import { Briefcase, Changeset, iModel, iModelsClient, RequestContext } from "@itwin/imodels-client-authoring";
-import { Config, TestiModelMetadata } from ".";
-import { toArray } from "./CommonTestUtils";
+import { Briefcase, Changeset, CheckpointState, iModel, iModelsClient, NamedVersion, RequestContext } from "@itwin/imodels-client-authoring";
+import { Config, TestAuthenticationProvider, TestiModelMetadata } from ".";
+import { sleep, TestSetupError, toArray } from "./CommonTestUtils";
 
 export interface TestiModelBriefcase {
   id: number;
@@ -15,6 +15,12 @@ export interface TestiModelChangeset {
   containingChanges: number;
 }
 
+export interface TestiModelNamedVersion {
+  id: string;
+  changesetId: string;
+  changesetIndex: number;
+}
+
 export interface EmptyTestiModel {
   id: string;
   name: string;
@@ -26,14 +32,18 @@ export interface TestiModelWithChangesets extends EmptyTestiModel {
   changesets: TestiModelChangeset[];
 }
 
+export interface TestiModelWithChangesetsAndNamedVersions extends TestiModelWithChangesets {
+  namedVersions: TestiModelNamedVersion[];
+}
+
 export class TestiModelProvider {
-  private static _reusableiModel: TestiModelWithChangesets | undefined;
+  private static _reusableiModel: TestiModelWithChangesetsAndNamedVersions | undefined;
 
   public static async getOrCreateReusable(params: {
     imodelsClient: iModelsClient,
     requestContext: RequestContext,
     projectId: string
-  }): Promise<TestiModelWithChangesets> {
+  }): Promise<TestiModelWithChangesetsAndNamedVersions> {
     if (TestiModelProvider._reusableiModel)
       return TestiModelProvider._reusableiModel;
 
@@ -43,8 +53,8 @@ export class TestiModelProvider {
     };
 
     TestiModelProvider._reusableiModel =
-      await TestiModelProvider.getTestiModel(paramsWithiModelName) ??
-      await TestiModelProvider.createWithChangesets(paramsWithiModelName);
+      await TestiModelProvider.queryWithRelatedData(paramsWithiModelName) ??
+      await TestiModelProvider.createWithChangesetsAndNamedVersions(paramsWithiModelName);
 
     return TestiModelProvider._reusableiModel!;
   }
@@ -116,19 +126,76 @@ export class TestiModelProvider {
     }
   }
 
-  private static async getTestiModel(params: {
+  public static async createWithChangesetsAndNamedVersions(params: {
     imodelsClient: iModelsClient,
     requestContext: RequestContext,
     projectId: string,
     imodelName: string
-  }): Promise<TestiModelWithChangesets | undefined> {
+  }): Promise<TestiModelWithChangesetsAndNamedVersions> {
+
+    const imodel = await TestiModelProvider.createWithChangesets(params);
+
+    // We use this specific user for named version creation to mimic production
+    // environment since this user is able to generate checkpoints.
+    const requestContextForUser2 = await TestAuthenticationProvider.getRequestContext(Config.get().testUsers.user2);
+    const imodelScopedRequestParams = {
+      imodelsClient: params.imodelsClient,
+      requestContext: requestContextForUser2,
+      imodelId: imodel.id
+    };
+    const testNamedVersion1 = await TestiModelProvider.createNamedVersionOnChangesetIndex({ ...imodelScopedRequestParams, changesetIndex: 5 });
+    const testNamedVersion2 = await TestiModelProvider.createNamedVersionOnChangesetIndex({ ...imodelScopedRequestParams, changesetIndex: 10 });
+
+    await Promise.all([
+      TestiModelProvider.waitForNamedVersionCheckpointGenerated({ ...imodelScopedRequestParams, namedVersionId: testNamedVersion1.id }),
+      TestiModelProvider.waitForNamedVersionCheckpointGenerated({ ...imodelScopedRequestParams, namedVersionId: testNamedVersion2.id })
+    ]);
+
+    return {
+      ...imodel,
+      namedVersions: [testNamedVersion1, testNamedVersion2]
+    }
+  }
+
+  private static async createNamedVersionOnChangesetIndex(params: {
+    imodelsClient: iModelsClient,
+    requestContext: RequestContext,
+    imodelId: string,
+    changesetIndex: number
+  }): Promise<TestiModelNamedVersion> {
+    const changesetMetadata = TestiModelMetadata.Changesets[params.changesetIndex - 1];
+    const namedVersion = await params.imodelsClient.NamedVersions.create({
+      requestContext: params.requestContext,
+      imodelId: params.imodelId,
+      namedVersionProperties: {
+        name: `Named version ${changesetMetadata.index}`,
+        changesetId: changesetMetadata.id
+      }
+    });
+    return {
+      id: namedVersion.id,
+      changesetId: changesetMetadata.id,
+      changesetIndex: changesetMetadata.index
+    }
+  }
+
+  private static async queryWithRelatedData(params: {
+    imodelsClient: iModelsClient,
+    requestContext: RequestContext,
+    projectId: string,
+    imodelName: string
+  }): Promise<TestiModelWithChangesetsAndNamedVersions | undefined> {
     const existingiModel = await TestiModelProvider.findiModelByName(params);
     if (!existingiModel)
       return undefined;
 
-    const imodelScopedRequestParams = { requestContext: params.requestContext, imodelId: existingiModel.id };
+    const imodelScopedRequestParams = {
+      requestContext: params.requestContext,
+      imodelId: existingiModel.id
+    };
     const briefcase: Briefcase = (await toArray(params.imodelsClient.Briefcases.getRepresentationList(imodelScopedRequestParams)))[0];
     const changesets: Changeset[] = await toArray(params.imodelsClient.Changesets.getRepresentationList(imodelScopedRequestParams));
+    const namedVersions: NamedVersion[] = await toArray(params.imodelsClient.NamedVersions.getRepresentationList(imodelScopedRequestParams));
 
     return {
       id: existingiModel.id,
@@ -138,7 +205,14 @@ export class TestiModelProvider {
         id: briefcase.briefcaseId,
         deviceName: briefcase.deviceName!
       },
-      changesets
+      changesets,
+      namedVersions: namedVersions.map(nv => {
+        return {
+          id: nv.id,
+          changesetId: nv.changesetId!,
+          changesetIndex: TestiModelMetadata.Changesets.find(cs => cs.id === nv.changesetId)!.index
+        }
+      })
     }
   }
 
@@ -162,5 +236,28 @@ export class TestiModelProvider {
     }
 
     return undefined;
+  }
+
+  private static async waitForNamedVersionCheckpointGenerated(params: {
+    imodelsClient: iModelsClient,
+    requestContext: RequestContext,
+    imodelId: string,
+    namedVersionId: string
+  }): Promise<void> {
+    const sleepPeriodInMs = 1000;
+    const timeOutInMs = 5 * 60 * 1000;
+    for (let retries = timeOutInMs / sleepPeriodInMs; retries > 0; --retries) {
+      const checkpoint = await params.imodelsClient.Checkpoints.getByNamedVersionId(params);
+
+      if (checkpoint.state === CheckpointState.Successful)
+        return;
+
+      if (checkpoint.state !== CheckpointState.Scheduled)
+        throw new TestSetupError(`Checkpoint generation failed with state: ${checkpoint.state}.`);
+
+      await sleep(sleepPeriodInMs);
+    }
+
+    throw new TestSetupError(`Timed out while waiting for checkpoint generation to complete.`);
   }
 }
