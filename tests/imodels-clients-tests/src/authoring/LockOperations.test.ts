4dc34d8ee89fd2ed42aeabb4f155d2b86f92d284
Austeja Kalpakovaite <austeja.kalpakovaite@bentley.com>
2021-11-10 09:50:40 +0200

    Added a basic test to fix the build

--- tests/imodels-clients-tests/src/authoring/LockOperations.test.ts
+++ tests/imodels-clients-tests/src/authoring/LockOperations.test.ts
@@ -1,11 +1,13 @@
-import { AuthorizationCallback, iModelsClient } from "@itwin/imodels-client-authoring";
-import { Config, Constants, TestAuthorizationProvider, TestClientOptions, TestiModelGroup, TestProjectProvider } from "../common";
+import { AuthorizationCallback, GetLockListParams, iModelsClient, toArray } from "@itwin/imodels-client-authoring";
+import { expect } from "chai";
+import { Config, Constants, iModelMetadata, TestAuthorizationProvider, TestClientOptions, TestiModelCreator, TestiModelGroup, TestProjectProvider } from "../common";
 
 describe("[Authoring] LockOperations", () => {
   let imodelsClient: iModelsClient;
   let authorization: AuthorizationCallback;
   let projectId: string;
   let testiModelGroup: TestiModelGroup;
+  let testiModel: iModelMetadata;
 
   before(async () => {
     imodelsClient = new iModelsClient(new TestClientOptions());
@@ -17,13 +19,26 @@ describe("[Authoring] LockOperations", () => {
         testSuite: "AuthoringLockOperations"
       }
     });
+    testiModel = await TestiModelCreator.createEmpty({
+      authorization,
+      imodelsClient,
+      projectId,
+      imodelName: testiModelGroup.getPrefixediModelName("Test iModel for write")
+    });
   });
 
-  it ("should get lock list", () => {
+  it("should get lock list", async () => {
     // Arrange
+    const getLockListParams: GetLockListParams = {
+      authorization,
+      imodelId: testiModel.id
+    }
 
     // Act
+    const locks = imodelsClient.Locks.getList(getLockListParams);
 
-    // Asert
+    // Assert
+    const lockArray = await toArray(locks);
+    expect(lockArray).length.to.equal(0);
   });
 });
