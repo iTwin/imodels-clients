c10b9b0385d84e68a93a686cffed91e2626750b6
Austeja Kalpakovaite <austeja.kalpakovaite@bentley.com>
2021-11-10 10:54:41 +0200

    Added more possible enum values to iModelsErrorCode

--- clients/imodels-client-management/src/base/interfaces/iModelsErrorInterfaces.ts
+++ clients/imodels-client-management/src/base/interfaces/iModelsErrorInterfaces.ts
@@ -5,19 +5,42 @@
 export enum iModelsErrorCode {
   Unrecognized = "Unrecognized",
 
-  // Common errors returned by API
+  // Errors returned by API
   Unknown = "Unknown",
   Unauthorized = "Unauthorized",
-
-  // Operation specific errors returned by the API
+  InsufficientPermissions = "InsufficientPermissions",
+  RateLimitExceeded = "RateLimitExceeded",
+  TooManyRequests = "TooManyRequests",
   InvalidiModelsRequest = "InvalidiModelsRequest",
+  RequestTooLarge = "RequestTooLarge",
+  ResourceQuotaExceeded = "ResourceQuotaExceeded",
+  DataConflict = "DataConflict",
+  MutuallyExclusivePropertiesProvided = "MutuallyExclusivePropertiesProvided",
+  MissingRequiredProperty = "MissingRequiredProperty",
+  MissingRequiredParameter = "MissingRequiredParameter",
+  MissingRequiredHeader = "MissingRequiredHeader",
   InvalidValue = "InvalidValue",
+  InvalidHeaderValue = "InvalidHeaderValue",
+  InvalidRequestBody = "InvalidRequestBody",
+  MissingRequestBody = "MissingRequestBody",
+  ConflictWithAnotherUser = "ConflictWithAnotherUser",
+  InvalidChange = "InvalidChange",
   iModelExists = "iModelExists",
-  FileNotFound = "FileNotFound",
+  VersionExists = "NamedVersionExists",
+  ChangesetExists = "ChangesetExists",
+  NamedVersionOnChangesetExists = "NamedVersionOnChangesetExists",
+  ProjectNotFound = "ProjectNotFound",
   iModelNotFound = "iModelNotFound",
+  VersionNotFound = "NamedVersionNotFound",
   ChangesetNotFound = "ChangesetNotFound",
-  NamedVersionNotFound = "NamedVersionNotFound",
+  UserNotFound = "UserNotFound",
+  BriefcaseNotFound = "BriefcaseNotFound",
+  MaximumNumberOfBriefcasesPerUser = "MaximumNumberOfBriefcasesPerUser",
+  FileNotFound = "FileNotFound",
+  BaselineFileNotFound = "BaselineFileNotFound",
   CheckpointNotFound = "CheckpointNotFound",
+  NewerChangesExist = "NewerChangesExist",
+  InvalidThumbnailFormat = "InvalidThumbnailFormat",
 
   // Operation specific errors thrown by the client library
   BaselineFileInitializationFailed = "BaselineFileInitializationFailed",
