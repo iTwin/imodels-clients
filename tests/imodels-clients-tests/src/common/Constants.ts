3e0e800b8b29abc887b0d76bed6cdbb0d057186c
Austeja Kalpakovaite <austeja.kalpakovaite@bentley.com>
2021-11-11 16:49:53 +0200

    WIP: attempt to fix path issues

--- tests/imodels-clients-tests/src/common/Constants.ts
+++ tests/imodels-clients-tests/src/common/Constants.ts
@@ -2,8 +2,10 @@
  * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
  * See LICENSE.md in the project root for license terms and full copyright notice.
  *--------------------------------------------------------------------------------------------*/
+import * as path from "path";
+
 export class Constants {
-  static TestDownloadDirectoryPath = `${__dirname}/../testDownloads/`;
+  static TestDownloadDirectoryPath = path.join(__dirname, "../testDownloads");
   static PackagePrefix = "iModelsClientsTests";
   static TestDeviceName = "Test device";
 }
