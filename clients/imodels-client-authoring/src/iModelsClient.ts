114963fcd26f59fd4ce9ad4db6cf85683f23b207
Austeja Kalpakovaite <austeja.kalpakovaite@bentley.com>
2021-12-02 17:43:20 +0200

    iModelsClient class documentation

--- clients/imodels-client-authoring/src/iModelsClient.ts
+++ clients/imodels-client-authoring/src/iModelsClient.ts
@@ -14,13 +14,28 @@ import { BriefcaseOperations, ChangesetOperations, LockOperations, iModelOperati
 import { iModelsApiUrlFormatter } from "./operations/iModelsApiUrlFormatter";
 import { OperationOptions } from "./operations/OperationOptions";
 
+/** User-configurable iModels client options. */
 export interface iModelsClientOptions extends ManagementiModelsClientOptions {
+  /**
+   * File handler to use in operations which transfer files. Examples of such operations are Changeset download in 
+   * {@link ChangesetOperations}, iModel creation from Baseline in {@link iModelOperations}. If `undefined` the default
+   * handler is used which is implemented using Azure SDK. See {@link AzureSdkFileHandler}.
+   */
   fileHandler?: FileHandler;
 }
 
+/**
+ * iModels API client for iModel authoring workflows. For more information on the API visit the
+ * {@link https://developer.bentley.com/apis/imodels/ iModels API documentation page}.
+ */
 export class iModelsClient {
   private _operationsOptions: OperationOptions;
 
+  /** 
+   * Class constructor. 
+   * @param {iModelsClientOptions} options client options. If `options` are `undefined` or if some of the properties
+   * are `undefined` the client uses defaults. See {@link iModelsClientOptions}.
+   */
   constructor(options?: iModelsClientOptions) {
     const fillediModelsClientOptions = iModelsClient.fillConfiguration(options);
     this._operationsOptions = {
@@ -29,34 +44,49 @@ export class iModelsClient {
     };
   }
 
+  /** 
+   * File handler that is used for file transfer operations. The handler is optionally configurable by user, 
+   * see {@link iModelsClientOptions}.
+   */
   public get FileHandler(): FileHandler {
     return this._operationsOptions.fileHandler;
   }
 
+  /** iModel operations. See {@link iModelOperations}. */
   public get iModels(): iModelOperations<OperationOptions> {
     return new iModelOperations(this._operationsOptions);
   }
 
+  /** Briefcase operations. See {@link BriefcaseOperations}. */
   public get Briefcases(): BriefcaseOperations<OperationOptions> {
     return new BriefcaseOperations(this._operationsOptions);
   }
 
+  /** Changeset operations. See {@link ChangesetOperations}. */
   public get Changesets(): ChangesetOperations<OperationOptions> {
     return new ChangesetOperations(this._operationsOptions, this.NamedVersions, this.Checkpoints);
   }
 
+  /** Named version operations. See {@link NamedVersionOperations}. */
   public get NamedVersions(): NamedVersionOperations<OperationOptions> {
     return new NamedVersionOperations(this._operationsOptions);
   }
 
+  /** Checkpoint operations. See {@link CheckpointOperations}. */
   public get Checkpoints(): CheckpointOperations<OperationOptions> {
     return new CheckpointOperations(this._operationsOptions);
   }
 
+  /** Lock operations. See {@link LockOperations}. */
   public get Locks(): LockOperations<OperationOptions> {
     return new LockOperations(this._operationsOptions);
   }
 
+  /** 
+   * Creates a configuration from user-specified properties and default values. See {@link iModelsClientOptions}.
+   * @param {iModelsClientOptions} options user-passed client options.
+   * @returns {RecursiveRequired<iModelsClientOptions>} options with all the gaps filled with default values.
+   */
   public static fillConfiguration(options?: iModelsClientOptions): RecursiveRequired<iModelsClientOptions> {
     return {
       ...ManagementiModelsClient.fillConfiguration(options),
--- clients/imodels-client-management/src/iModelsClient.ts
+++ clients/imodels-client-management/src/iModelsClient.ts
@@ -9,19 +9,37 @@ import { CheckpointOperations } from "./operations/checkpoint/CheckpointOperatio
 import { iModelsApiUrlFormatter } from "./operations/iModelsApiUrlFormatter";
 import { OperationOptions } from "./operations/OperationOptions";
 
+/** iModels API endpoint options. */
 export interface ApiOptions {
+  /** iModels API base url. Default value is `https://api.bentley.com/imodels`. */
   baseUri?: string;
+  /** iModels API version. Default value is `itwin-platform.v1`. */
   version?: string;
 }
 
+/** User-configurable iModels client options. */
 export interface iModelsClientOptions {
+  /** 
+   * Rest client that is used for making HTTP requests. If `undefined` the default client is used which is implemented
+   * using `axios` library. See {@link AxiosRestClient}.
+   */
   restClient?: RestClient;
+  /** iModels API options. See {@link ApiOptions}. */
   api?: ApiOptions;
 }
 
+/**
+ * iModels API client for iModel management workflows. For more information on the API visit the
+ * {@link https://developer.bentley.com/apis/imodels/ iModels API documentation page}.
+ */
 export class iModelsClient {
   private _operationsOptions: OperationOptions;
 
+  /** 
+   * Class constructor. 
+   * @param {iModelsClientOptions} options client options. If `options` are `undefined` or if some of the properties
+   * are `undefined` the client uses defaults. See {@link iModelsClientOptions}.
+   */
   constructor(options?: iModelsClientOptions) {
     const fillediModelsClientOptions = iModelsClient.fillConfiguration(options);
     this._operationsOptions = {
@@ -30,26 +48,36 @@ export class iModelsClient {
     };
   }
 
+  /** iModel operations. See {@link iModelOperations}. */
   public get iModels(): iModelOperations<OperationOptions> {
     return new iModelOperations(this._operationsOptions);
   }
 
+  /** Briefcase operations. See {@link BriefcaseOperations}. */
   public get Briefcases(): BriefcaseOperations<OperationOptions> {
     return new BriefcaseOperations(this._operationsOptions);
   }
 
+  /** Changeset operations. See {@link ChangesetOperations}. */
   public get Changesets(): ChangesetOperations<OperationOptions> {
     return new ChangesetOperations(this._operationsOptions, this.NamedVersions, this.Checkpoints);
   }
 
+  /** Named version operations. See {@link NamedVersionOperations}. */
   public get NamedVersions(): NamedVersionOperations<OperationOptions> {
     return new NamedVersionOperations(this._operationsOptions);
   }
 
+  /** Checkpoint operations. See {@link CheckpointOperations}. */
   public get Checkpoints(): CheckpointOperations<OperationOptions> {
     return new CheckpointOperations(this._operationsOptions);
   }
 
+  /** 
+   * Creates a configuration from user-specified properties and default values. See {@link iModelsClientOptions}.
+   * @param {iModelsClientOptions} options user-passed client options.
+   * @returns {RecursiveRequired<iModelsClientOptions>} options with all the gaps filled with default values.
+   */
   public static fillConfiguration(options?: iModelsClientOptions): RecursiveRequired<iModelsClientOptions> {
     return {
       restClient: options?.restClient ?? new AxiosRestClient(),
