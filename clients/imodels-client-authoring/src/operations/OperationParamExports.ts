6c171856abb25b638a4ed4186508c53f9697dc64
Austeja Kalpakovaite <austeja.kalpakovaite@bentley.com>
2021-12-07 12:28:36 +0200

    Another incorrect path fix

--- clients/imodels-client-authoring/src/operations/OperationParamExports.ts
+++ clients/imodels-client-authoring/src/operations/OperationParamExports.ts
@@ -2,7 +2,7 @@
  * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
  * See LICENSE.md in the project root for license terms and full copyright notice.
  *--------------------------------------------------------------------------------------------*/
-export * from "./iModel/IModelOperationParams";
+export * from "./imodel/IModelOperationParams";
 export * from "./briefcase/BriefcaseOperationParams";
 export * from "./changeset/ChangesetOperationParams";
 export * from "./lock/LockOperationParams";
