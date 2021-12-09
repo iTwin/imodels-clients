a4a2ae58d228305e4b35809ac446262fafcf053b
Austėja Kalpakovaitė <70565417+austeja-bentley@users.noreply.github.com>
2021-12-09 12:14:31 +0200

    Infrastructure for `BackendiModelsAccess` integration tests (#54)
    * WIP: adding backend interop tests
    
    * Completing test suite setup
    
    * Lint fixes
    
    * Removed comment
    
    * Removed cleanup and setup since they are not required now
    
    * Removed unnecessary export
    
    * Reverted another unnecessary change
    
    * Added tests to the pipeline
    
    * Reverted another change
    
    * PR fix: renaming pipeline step for consistency

---
+++ tests/imodels-access-backend-tests/src/BackendiModelsAccess.test.ts
@@ -0,0 +1,47 @@
+/*---------------------------------------------------------------------------------------------
+ * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
+ * See LICENSE.md in the project root for license terms and full copyright notice.
+ *--------------------------------------------------------------------------------------------*/
+import { IModelIdArg } from "@itwin/core-backend";
+import { BriefcaseId } from "@itwin/core-common";
+import { BackendIModelsAccess } from "@itwin/imodels-access-backend";
+import { expect } from "chai";
+import { IModelsClient } from "@itwin/imodels-client-authoring";
+import { Config, ReusableIModelMetadata, ReusableTestIModelProvider, TestAuthorizationProvider, TestClientOptions, TestProjectProvider } from "@itwin/imodels-clients-tests";
+
+describe("BackendiModelsAccess", () => {
+  let backendIModelsAccess: BackendIModelsAccess;
+  let accessToken: string;
+  let projectId: string;
+  let testIModel: ReusableIModelMetadata;
+
+  before(async () => {
+    const iModelsClient = new IModelsClient(new TestClientOptions());
+    const authorization = await TestAuthorizationProvider.getAuthorization(Config.get().testUsers.admin1);
+
+    backendIModelsAccess = new BackendIModelsAccess(iModelsClient);
+    accessToken = `${(await authorization()).scheme} ${(await authorization()).token}`;
+    projectId = await TestProjectProvider.getProjectId();
+    testIModel = await ReusableTestIModelProvider.getOrCreate({
+      iModelsClient,
+      authorization,
+      projectId
+    });
+  });
+
+  it("should get current user briefcase ids", async () => {
+    // Arrange
+    const getMyBriefcaseIdsParams: IModelIdArg = {
+      accessToken,
+      iModelId: testIModel.id
+    };
+
+    // Act
+    const briefcaseIds: BriefcaseId[] = await backendIModelsAccess.getMyBriefcaseIds(getMyBriefcaseIdsParams);
+
+    // Assert
+    expect(briefcaseIds.length).to.equal(1);
+    const briefcaseId = briefcaseIds[0];
+    expect(briefcaseId).to.equal(testIModel.briefcase.id);
+  });
+});
