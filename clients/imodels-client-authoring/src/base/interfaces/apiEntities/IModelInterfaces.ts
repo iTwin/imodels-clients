5f017cb35e352b6e212f068f572d9f8c4a291361
Austeja Kalpakovaite <austeja.kalpakovaite@bentley.com>
2021-12-30 10:46:24 +0200

    Finalizing documentation for interfaces and classes in authoring client

--- clients/imodels-client-authoring/src/base/interfaces/apiEntities/IModelInterfaces.ts
+++ clients/imodels-client-authoring/src/base/interfaces/apiEntities/IModelInterfaces.ts
@@ -4,11 +4,18 @@
  *--------------------------------------------------------------------------------------------*/
 import { IModel, Link } from "@itwin/imodels-client-management";
 
+/** DTO to hold links that belong to iModel entity returned from iModels API. */
 export interface IModelLinks {
+  /** Link where to upload the iModel Baseline file. Link points to a remote storage. */
   upload: Link;
+  /**
+   * Link to confirm the Baseline file upload and complete the iModel creation process. Points to a specific
+   * iModel Baseline instance in iModels API.
+   */
   complete: Link;
 }
 
+/** DTO to hold a single iModel API response returned by iModel creation operation. */
 export interface IModelCreateResponse {
   iModel: IModel & { _links: IModelLinks };
 }
