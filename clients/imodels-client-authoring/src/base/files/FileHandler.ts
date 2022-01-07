708729549911d46015435b4f02318183c28b848f
Austeja Kalpakovaite <austeja.kalpakovaite@bentley.com>
2022-01-07 13:34:13 +0200

    Documentation update

--- clients/imodels-client-authoring/src/base/files/FileHandler.ts
+++ clients/imodels-client-authoring/src/base/files/FileHandler.ts
@@ -42,47 +42,49 @@ export interface FileHandler {
   /**
    * Uploads file from the local source to the remote target and reports progress via the user-passed progress callback.
    * @param {UploadFileParams} params parameters for this operation. See {@link UploadFileParams}.
-   * @returns a promise that resolves after operation completes.
+   * @returns {Promise<void>} a promise that resolves after operation completes.
    */
   uploadFile(params: UploadFileParams): Promise<void>;
 
   /**
    * Downloads file from the remote target to the local source and reports progress via the user-passed progress callback.
    * @param {DownloadFileParams} params parameters for this operation. See {@link DownloadFileParams}.
-   * @returns a promise that resolves after operation completes.
+   * @returns {Promise<void>} a promise that resolves after operation completes.
    */
   downloadFile(params: DownloadFileParams): Promise<void>;
 
   /**
    * Determines if a file with the specified path exists in the file system.
    * @param {string} filePath path of the file.
-   * @returns `true` if the file exists, `false` otherwise.
+   * @returns {Promise<boolean>} `true` if the file exists, `false` otherwise.
    */
   exists(filePath: string): Promise<boolean>;
 
   /**
    * Determines size of the specified file.
    * @param {string} filePath path of the file.
-   * @returns file size in bytes.
+   * @returns {Promise<number>} file size in bytes.
    */
   getFileSize(filePath: string): Promise<number>;
 
   /**
    * Deletes the specified file.
    * @param {string} filePath path of the file.
+   * @returns {Promise<void>} a promise that resolves after operation completes.
    */
   unlink(filePath: string): Promise<void>;
 
   /**
    * Creates specified directory recursively.
    * @param {string} directoryPath directory to create.
+   * @returns {Promise<void>} a promise that resolves after operation completes.
    */
   createDirectory(directoryPath: string): Promise<void>;
 
   /**
    * Joins all path segments together into a normalized path.
    * @param {string[]} paths path segments to join.
-   * @returns normalized path.
+   * @returns {Promise<string>} normalized path.
    */
   join(...paths: string[]): Promise<string>;
 }
