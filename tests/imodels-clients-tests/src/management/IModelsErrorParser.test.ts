7cdf408b90388698b1b0238d26b68288eb854fd9
Austeja Kalpakovaite <austeja.kalpakovaite@bentley.com>
2022-05-17 13:07:38 +0300

    Reverted accidental changes

--- tests/imodels-clients-tests/src/management/IModelsErrorParser.test.ts
+++ tests/imodels-clients-tests/src/management/IModelsErrorParser.test.ts
@@ -32,7 +32,7 @@ describe("IModelsErrorParser", () => {
     };
 
     // Act
-    const parsedError: IModelsError = IModelsErrorParser.parse({ statusCode: 400, body: errorResponse }) ;
+    const parsedError: IModelsError = IModelsErrorParser.parse({ statusCode: 400, body: errorResponse }) as IModelsError;
 
     // Assert
     const expectedErrorMessage = "Cannot create iModel. Details:\n" +
