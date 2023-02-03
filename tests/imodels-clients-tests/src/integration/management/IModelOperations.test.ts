251148734038841b8bbd4a33ca779665edc33324
nick.tessier <22119573+nick4598@users.noreply.github.com>
2023-02-03 09:13:26 -0500

    fix error message

--- tests/imodels-clients-tests/src/integration/management/IModelOperations.test.ts
+++ tests/imodels-clients-tests/src/integration/management/IModelOperations.test.ts
@@ -413,7 +413,7 @@ describe("[Management] IModelOperations", () => {
       objectThrown,
       expectedError: {
         code: IModelsErrorCode.InvalidIModelsRequest,
-        message: "Cannot create iModel. Details:\n1. InvalidValue: Provided 'description' is not valid. The value exceeds allowed 255 characters. Target: description.\n",
+        message: "Cannot create iModel. Details:\n1. InvalidValue: Provided 'description' value is not valid. The value exceeds allowed 255 characters. Target: description.\n",
         details: [{
           code: IModelsErrorCode.InvalidValue,
           message: "Provided 'description' is not valid. The value exceeds allowed 255 characters.",
