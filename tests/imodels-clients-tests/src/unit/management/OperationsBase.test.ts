187ce8dbb47184634bd4f9fed70ab1eee451c568
Denisas Savickis <denisijus@gmail.com>
2023-06-06 10:51:37 +0300

    Fixing content-type header being present for empty body requests

--- tests/imodels-clients-tests/src/unit/management/OperationsBase.test.ts
+++ tests/imodels-clients-tests/src/unit/management/OperationsBase.test.ts
@@ -15,82 +15,6 @@ describe("[Management] OperationsBase", () => {
   let restClient: sinon.SinonStubbedInstance<AxiosRestClient>;
   let requiredArgs: {url: string, authorization: () => Promise<Authorization>};
 
-  const operationsTestCases: {
-    caseName: string;
-    headersPassedToConstructor: Record<string, string | (() => string | undefined)>;
-    headersPassedToOperation: Record<string, string | (() => string | undefined)>;
-    expectedHeaders: Record<string, string>;
-  }[] = [
-    {
-      caseName: "Adds constructor headers",
-      headersPassedToConstructor: {
-        "header-specified-in-constructor": "constructor"
-      },
-      headersPassedToOperation: {},
-      expectedHeaders: {
-        "header-specified-in-constructor": "constructor"
-      }
-    },
-    {
-      caseName: "Adds operation headers",
-      headersPassedToConstructor: {},
-      headersPassedToOperation: {
-        "header-specified-in-operation": "operation"
-      },
-      expectedHeaders: {
-        "header-specified-in-operation": "operation"
-      }
-    },
-    {
-      caseName: "Adds constructor and operation headers",
-      headersPassedToConstructor: {
-        "header-specified-in-constructor": "constructor"
-      },
-      headersPassedToOperation: {
-        "header-specified-in-operation": "operation"
-      },
-      expectedHeaders: {
-        "header-specified-in-constructor": "constructor",
-        "header-specified-in-operation": "operation"
-      }
-    },
-    {
-      caseName: "Replaces constructor headers",
-      headersPassedToConstructor: {
-        "header-specified-in-constructor": "constructor"
-      },
-      headersPassedToOperation: {
-        "header-specified-in-constructor": "operation"
-      },
-      expectedHeaders: {
-        "header-specified-in-constructor": "operation"
-      }
-    },
-    {
-      caseName: "Removes constructor headers",
-      headersPassedToConstructor: {
-        "header-specified-in-constructor": "constructor"
-      },
-      headersPassedToOperation: {
-        "header-specified-in-constructor": () => undefined
-      },
-      expectedHeaders: {}
-    },
-    {
-      caseName: "Works with function headers",
-      headersPassedToConstructor: {
-        "header-specified-in-constructor": () => "constructor"
-      },
-      headersPassedToOperation: {
-        "header-specified-in-operation": () => "operation"
-      },
-      expectedHeaders: {
-        "header-specified-in-constructor": "constructor",
-        "header-specified-in-operation": "operation"
-      }
-    }
-  ];
-
   before( async () => {
     restClient = createStub(AxiosRestClient);
     const authorizationCallback = async () => {
@@ -103,111 +27,320 @@ describe("[Management] OperationsBase", () => {
     requiredArgs = {url: "url", authorization: authorizationCallback};
   });
 
-  for( const testCase of operationsTestCases) {
-    const { caseName, headersPassedToConstructor, headersPassedToOperation, expectedHeaders } = testCase;
+  describe("OperationsBase headers", () => {
+    const operationsTestCases: {
+      caseName: string;
+      headersPassedToConstructor: Record<string, string | (() => string | undefined)>;
+      headersPassedToOperation: Record<string, string | (() => string | undefined)>;
+      expectedHeaders: Record<string, string>;
+    }[] = [
+      {
+        caseName: "Adds constructor headers",
+        headersPassedToConstructor: {
+          "header-specified-in-constructor": "constructor"
+        },
+        headersPassedToOperation: {},
+        expectedHeaders: {
+          "header-specified-in-constructor": "constructor"
+        }
+      },
+      {
+        caseName: "Adds operation headers",
+        headersPassedToConstructor: {},
+        headersPassedToOperation: {
+          "header-specified-in-operation": "operation"
+        },
+        expectedHeaders: {
+          "header-specified-in-operation": "operation"
+        }
+      },
+      {
+        caseName: "Adds constructor and operation headers",
+        headersPassedToConstructor: {
+          "header-specified-in-constructor": "constructor"
+        },
+        headersPassedToOperation: {
+          "header-specified-in-operation": "operation"
+        },
+        expectedHeaders: {
+          "header-specified-in-constructor": "constructor",
+          "header-specified-in-operation": "operation"
+        }
+      },
+      {
+        caseName: "Replaces constructor headers",
+        headersPassedToConstructor: {
+          "header-specified-in-constructor": "constructor"
+        },
+        headersPassedToOperation: {
+          "header-specified-in-constructor": "operation"
+        },
+        expectedHeaders: {
+          "header-specified-in-constructor": "operation"
+        }
+      },
+      {
+        caseName: "Removes constructor headers",
+        headersPassedToConstructor: {
+          "header-specified-in-constructor": "constructor"
+        },
+        headersPassedToOperation: {
+          "header-specified-in-constructor": () => undefined
+        },
+        expectedHeaders: {}
+      },
+      {
+        caseName: "Works with function headers",
+        headersPassedToConstructor: {
+          "header-specified-in-constructor": () => "constructor"
+        },
+        headersPassedToOperation: {
+          "header-specified-in-operation": () => "operation"
+        },
+        expectedHeaders: {
+          "header-specified-in-constructor": "constructor",
+          "header-specified-in-operation": "operation"
+        }
+      }
+    ];
 
-    it(`${caseName} works with Get Request`, async () => {
+    for( const testCase of operationsTestCases) {
+      const { caseName, headersPassedToConstructor, headersPassedToOperation, expectedHeaders } = testCase;
+
+      it(`${caseName} works with Get Request`, async () => {
       // Arrange
-      const iModelsOperationsWrapper = new TestOperationsWrapper(
-        {
-          restClient,
-          api: {version: "version"},
-          headers: headersPassedToConstructor
+        const iModelsOperationsWrapper = new TestOperationsWrapper(
+          {
+            restClient,
+            api: {version: "version"},
+            headers: headersPassedToConstructor
+          });
+
+        // Act
+        await iModelsOperationsWrapper.sendGetRequest({
+          ...requiredArgs,
+          headers: headersPassedToOperation
         });
 
-      // Act
-      await iModelsOperationsWrapper.sendGetRequest({
-        ...requiredArgs,
-        headers: headersPassedToOperation
+        // Assert
+        const callHeaders = restClient.sendGetRequest.lastCall.args[0].headers;
+        expect(callHeaders).to.include(expectedHeaders);
       });
 
-      // Assert
-      const callHeaders = restClient.sendGetRequest.lastCall.args[0].headers;
-      expect(callHeaders).to.include(expectedHeaders);
-    });
+      it(`${caseName} works with Post Request`, async () => {
+      // Arrange
+        const iModelsOperationsWrapper = new TestOperationsWrapper(
+          {
+            restClient,
+            api: {version: "version"},
+            headers: headersPassedToConstructor
+          });
 
-    it(`${caseName} works with Post Request`, async () => {
+        // Act
+        await iModelsOperationsWrapper.sendPostRequest({
+          ...requiredArgs,
+          headers: headersPassedToOperation,
+          body: {}
+        });
+
+        // Assert
+        const callHeaders = restClient.sendPostRequest.lastCall.args[0].headers;
+        expect(callHeaders).to.include(expectedHeaders);
+      });
+
+      it(`${caseName} works with Put Request`, async () => {
+      // Arrange
+        const iModelsOperationsWrapper = new TestOperationsWrapper(
+          {
+            restClient,
+            api: {version: "version"},
+            headers: headersPassedToConstructor
+          });
+
+        // Act
+        await iModelsOperationsWrapper.sendPutRequest({
+          ...requiredArgs,
+          headers: headersPassedToOperation,
+          body: new Uint8Array(),
+          contentType: ContentType.Png
+        });
+
+        // Assert
+        const callHeaders = restClient.sendPutRequest.lastCall.args[0].headers;
+        expect(callHeaders).to.include(expectedHeaders);
+      });
+
+      it(`${caseName} works with Delete Request`, async () => {
+      // Arrange
+        const iModelsOperationsWrapper = new TestOperationsWrapper(
+          {
+            restClient,
+            api: {version: "version"},
+            headers: headersPassedToConstructor
+          });
+
+        // Act
+        await iModelsOperationsWrapper.sendDeleteRequest({
+          ...requiredArgs,
+          headers: headersPassedToOperation
+        });
+
+        // Assert
+        const callHeaders = restClient.sendDeleteRequest.lastCall.args[0].headers;
+        expect(callHeaders).to.include(expectedHeaders);
+      });
+
+      it(`${caseName} works with Patch Request`, async () => {
+      // Arrange
+        const iModelsOperationsWrapper = new TestOperationsWrapper(
+          {
+            restClient,
+            api: {version: "version"},
+            headers: headersPassedToConstructor
+          });
+
+        // Act
+        await iModelsOperationsWrapper.sendPatchRequest({
+          ...requiredArgs,
+          headers: headersPassedToOperation,
+          body: {}
+        });
+
+        // Assert
+        const callHeaders = restClient.sendPatchRequest.lastCall.args[0].headers;
+        expect(callHeaders).to.include(expectedHeaders);
+      });
+    }
+  });
+
+  describe("OperationsBase Content-Type", () => {
+
+    it("Adds Content-Type header for Post Request", async () => {
       // Arrange
       const iModelsOperationsWrapper = new TestOperationsWrapper(
         {
           restClient,
           api: {version: "version"},
-          headers: headersPassedToConstructor
+          headers: {}
         });
 
       // Act
       await iModelsOperationsWrapper.sendPostRequest({
         ...requiredArgs,
-        headers: headersPassedToOperation,
-        body: {}
+        body: {
+          contentType: ContentType.Json,
+          content: "serialized content"
+        }
       });
 
       // Assert
       const callHeaders = restClient.sendPostRequest.lastCall.args[0].headers;
-      expect(callHeaders).to.include(expectedHeaders);
+      expect(callHeaders).to.include({"Content-Type": ContentType.Json});
     });
 
-    it(`${caseName} works with Put Request`, async () => {
+    it("Does not add Content-Type header for Post Request", async () => {
       // Arrange
       const iModelsOperationsWrapper = new TestOperationsWrapper(
         {
           restClient,
           api: {version: "version"},
-          headers: headersPassedToConstructor
+          headers: {}
+        });
+
+      // Act
+      await iModelsOperationsWrapper.sendPostRequest({
+        ...requiredArgs,
+        body: {}
+      });
+
+      // Assert
+      const callHeaders = restClient.sendPostRequest.lastCall.args[0].headers;
+      expect(callHeaders).to.not.have.property("Content-Type");
+    });
+
+    it("Adds Content-Type header for Put Request", async () => {
+      // Arrange
+      const iModelsOperationsWrapper = new TestOperationsWrapper(
+        {
+          restClient,
+          api: {version: "version"},
+          headers: {}
         });
 
       // Act
       await iModelsOperationsWrapper.sendPutRequest({
         ...requiredArgs,
-        headers: headersPassedToOperation,
+        body: new Uint8Array(1),
+        contentType: ContentType.Png
+      });
+
+      // Assert
+      const callHeaders = restClient.sendPutRequest.lastCall.args[0].headers;
+      expect(callHeaders).to.include({"Content-Type": ContentType.Png});
+    });
+
+    it("Does not add Content-Type header for Put Request", async () => {
+      // Arrange
+      const iModelsOperationsWrapper = new TestOperationsWrapper(
+        {
+          restClient,
+          api: {version: "version"},
+          headers: {}
+        });
+
+      // Act
+      await iModelsOperationsWrapper.sendPutRequest({
+        ...requiredArgs,
         body: new Uint8Array(),
         contentType: ContentType.Png
       });
 
       // Assert
       const callHeaders = restClient.sendPutRequest.lastCall.args[0].headers;
-      expect(callHeaders).to.include(expectedHeaders);
+      expect(callHeaders).to.not.have.property("Content-Type");
     });
 
-    it(`${caseName} works with Delete Request`, async () => {
+    it("Adds Content-Type header for Patch Request", async () => {
       // Arrange
       const iModelsOperationsWrapper = new TestOperationsWrapper(
         {
           restClient,
           api: {version: "version"},
-          headers: headersPassedToConstructor
-        });
-
-      // Act
-      await iModelsOperationsWrapper.sendDeleteRequest({
-        ...requiredArgs,
-        headers: headersPassedToOperation
-      });
-
-      // Assert
-      const callHeaders = restClient.sendDeleteRequest.lastCall.args[0].headers;
-      expect(callHeaders).to.include(expectedHeaders);
-    });
-
-    it(`${caseName} works with Patch Request`, async () => {
-      // Arrange
-      const iModelsOperationsWrapper = new TestOperationsWrapper(
-        {
-          restClient,
-          api: {version: "version"},
-          headers: headersPassedToConstructor
+          headers: {}
         });
 
       // Act
       await iModelsOperationsWrapper.sendPatchRequest({
         ...requiredArgs,
-        headers: headersPassedToOperation,
+        body: {
+          contentType: ContentType.Json,
+          content: "serialized content"
+        }
+      });
+
+      // Assert
+      const callHeaders = restClient.sendPatchRequest.lastCall.args[0].headers;
+      expect(callHeaders).to.include({"Content-Type": ContentType.Json});
+    });
+
+    it("Does not add Content-Type header for Patch Request", async () => {
+      // Arrange
+      const iModelsOperationsWrapper = new TestOperationsWrapper(
+        {
+          restClient,
+          api: {version: "version"},
+          headers: {}
+        });
+
+      // Act
+      await iModelsOperationsWrapper.sendPatchRequest({
+        ...requiredArgs,
         body: {}
       });
 
       // Assert
       const callHeaders = restClient.sendPatchRequest.lastCall.args[0].headers;
-      expect(callHeaders).to.include(expectedHeaders);
+      expect(callHeaders).to.not.have.property("Content-Type");
     });
-  }
+  });
 });
