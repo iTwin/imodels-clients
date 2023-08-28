593082720ee98d2879675a7d2ad6ce2155a907ec
Austėja Kalpakovaitė <70565417+austeja-bentley@users.noreply.github.com>
2023-08-28 16:38:45 +0300

    `BackendIModelsAccess.downloadV1Checkpoint` fix: skip over Checkpoints V2 when searching for the preceding Checkpoint V1 (#190)
    * Add recursive checkpoint search for v1 checkpoint querying
    
    * Integration test
    
    * Lint fixes
    
    * Spelling fix
    
    * Changed assertion to assert null changeset id
    
    * Lint fix

---
+++ itwin-platform-access/imodels-access-backend/src/Constants.ts
@@ -0,0 +1,8 @@
+/*---------------------------------------------------------------------------------------------
+ * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
+ * See LICENSE.md in the project root for license terms and full copyright notice.
+ *--------------------------------------------------------------------------------------------*/
+
+export class Constants {
+  public static readonly ChangeSet0 = { id: "", changesType: 0, description: "initialChangeset", parentId: "", briefcaseId: 0, pushDate: "", userCreated: "", index: 0, size: 0 };
+}
