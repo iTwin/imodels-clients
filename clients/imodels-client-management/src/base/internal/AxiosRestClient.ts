5992c4571737388b39943dba055fd8073466808a
Arminas Brazenas <arminas.brazenas@gmail.com>
2024-03-20 15:21:23 +0200

    PR comment fixes

--- clients/imodels-client-management/src/base/internal/AxiosRestClient.ts
+++ clients/imodels-client-management/src/base/internal/AxiosRestClient.ts
@@ -4,8 +4,9 @@
  *--------------------------------------------------------------------------------------------*/
 import axios, { AxiosRequestConfig, AxiosResponse } from "axios";
 
-import { Dictionary } from "../types";
-import { ContentType, HttpGetRequestParams, HttpRequestParams, HttpRequestWithBinaryBodyParams, HttpRequestWithJsonBodyParams, HttpResponse, HttpResponseHeaders, RestClient } from "../types/RestClient";
+import { ContentType, HttpGetRequestParams, HttpRequestParams, HttpRequestWithBinaryBodyParams, HttpRequestWithJsonBodyParams, HttpResponse, RestClient } from "../types/RestClient";
+
+import { AxiosResponseHeadersAdapter } from "./AxiosResponseHeadersAdapter";
 
 /**
  * Function that is called if the HTTP request fails and which returns an error that will be thrown by one of the
@@ -13,19 +14,12 @@ import { ContentType, HttpGetRequestParams, HttpRequestParams, HttpRequestWithBi
  */
 export type ParseErrorFunc = (response: { statusCode?: number, body?: unknown }, originalError: Error & { code?: string }) => Error;
 
-/** A factory for creating {@link HttpResponseHeaders} from {@link AxiosResponse}. */
-export interface HttpResponseHeadersFactory {
-  create(response: AxiosResponse): HttpResponseHeaders;
-}
-
 /** Default implementation for {@link RestClient} interface that uses `axios` library for sending the requests. */
 export class AxiosRestClient implements RestClient {
   private _parseErrorFunc: ParseErrorFunc;
-  private _responseHeadersFactory: HttpResponseHeadersFactory;
 
-  constructor(parseErrorFunc: ParseErrorFunc, responseHeadersFactory: HttpResponseHeadersFactory) {
+  constructor(parseErrorFunc: ParseErrorFunc) {
     this._parseErrorFunc = parseErrorFunc;
-    this._responseHeadersFactory = responseHeadersFactory;
   }
 
   public sendGetRequest<TBody>(params: HttpGetRequestParams & { responseType: ContentType.Json }): Promise<HttpResponse<TBody>>;
@@ -87,7 +81,7 @@ export class AxiosRestClient implements RestClient {
 
       return {
         body: response.data,
-        headers: this._responseHeadersFactory.create(response)
+        headers: new AxiosResponseHeadersAdapter(response)
       };
     } catch (error: unknown) {
       if (axios.isAxiosError(error)) {
@@ -99,33 +93,3 @@ export class AxiosRestClient implements RestClient {
   }
 }
 
-/** Default implementation for {@link HttpResponseHeadersFactory}. */
-export class AxiosHeadersAdapterFactory implements HttpResponseHeadersFactory {
-  public create(response: AxiosResponse): HttpResponseHeaders {
-    return new AxiosHeadersAdapter(response);
-  }
-}
-
-/** Default implementation for {@link HttpResponseHeaders} interface, which adapts `axios` HTTP response headers to headers expected by the iModels Client. */
-export class AxiosHeadersAdapter implements HttpResponseHeaders {
-  private _response: AxiosResponse;
-
-  constructor(response: AxiosResponse) {
-    this._response = response;
-  }
-
-  public get(headerName: string): unknown {
-    // Directly manipulating headers object is deprecated.
-    // https://github.com/axios/axios?tab=readme-ov-file#-axiosheaders
-    if (this._response.headers.get instanceof Function)
-      return this._response.headers.get(headerName);
-
-    // It's most likely that header name is lowercase.
-    // https://axios-http.com/docs/res_schema
-    return this._response.headers[headerName.toLowerCase()];
-  }
-
-  public getAll(): Dictionary<unknown> {
-    return this._response.headers;
-  }
-}
--- tests/imodels-clients-tests/src/unit/management/AxiosRestClient.test.ts
+++ tests/imodels-clients-tests/src/unit/management/AxiosRestClient.test.ts
@@ -1,4 +1,8 @@
-import { AxiosHeadersAdapterFactory, AxiosRestClient, IModelsErrorParser } from "@itwin/imodels-client-management/lib/base/internal";
+/*---------------------------------------------------------------------------------------------
+ * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
+ * See LICENSE.md in the project root for license terms and full copyright notice.
+ *--------------------------------------------------------------------------------------------*/
+import { AxiosRestClient, IModelsErrorParser } from "@itwin/imodels-client-management/lib/base/internal";
 import axios from "axios";
 import MockAdapter from "axios-mock-adapter";
 import { expect } from "chai";
@@ -12,7 +16,7 @@ describe("[Management] AxiosRestClient", () => {
     axiosMock = new MockAdapter(axios);
   });
 
-  it("should get response header value by case-insensitive name", async () => {
+  it("should get response body", async () => {
     // Arrange
     const requestParams: HttpRequestWithJsonBodyParams = {
       url: "https://imodelhub.bentley.com",
@@ -23,24 +27,77 @@ describe("[Management] AxiosRestClient", () => {
       }
     };
     const responseBody = {
-      iModelId: "IMODEL_ID"
+      iModelId: "IMODEL_ID",
+      changesetId: 4,
+      _links: {
+        self: "https://imodelhub.bentley.com/something"
+      }
     };
-    const responseHeaders = {
-      "location": "https://some-url.com",
-      "retry-after": 60
-    };
-    axiosMock.onPost(requestParams.url).reply(200, responseBody, responseHeaders);
-    const restClient = new AxiosRestClient(IModelsErrorParser.parse, new AxiosHeadersAdapterFactory());
+    axiosMock.onPost(requestParams.url).reply(200, responseBody, {});
+    const restClient = new AxiosRestClient(IModelsErrorParser.parse);
 
     // Act
     const response = await restClient.sendPostRequest(requestParams);
 
     // Assert
     expect(response.body).to.deep.equal(responseBody);
+  });
+
+  it("should get response header value by case-insensitive name", async () => {
+    // Arrange
+    const requestParams: HttpRequestWithJsonBodyParams = {
+      url: "https://imodelhub.bentley.com",
+      headers: {},
+      body: {
+        content: {},
+        contentType: ContentType.Json
+      }
+    };
+    const responseHeaders = {
+      "location": "https://some-url.com",
+      "retry-after": 60
+    };
+    axiosMock.onPost(requestParams.url).reply(200, {}, responseHeaders);
+    const restClient = new AxiosRestClient(IModelsErrorParser.parse);
+
+    // Act & Assert
+    const response = await restClient.sendPostRequest(requestParams);
+
     expect(response.headers.get("location")).to.equal("https://some-url.com");
     expect(response.headers.get("Location")).to.equal("https://some-url.com");
     expect(response.headers.get("LOCATION")).to.equal("https://some-url.com");
     expect(response.headers.get("Retry-After")).to.equal("60");
     expect(response.headers.get("non-existent-header")).to.not.exist;
+  });
+
+  it("should get all response headers", async () => {
+    // Arrange
+    const requestParams: HttpRequestWithJsonBodyParams = {
+      url: "https://imodelhub.bentley.com",
+      headers: {},
+      body: {
+        content: {},
+        contentType: ContentType.Json
+      }
+    };
+    const responseHeaders = {
+      "location": "https://some-url.com",
+      "retry-after": 60,
+      "Content-Type": "application/json"
+    };
+    const expectedHeaders = {
+      "location": "https://some-url.com",
+      "retry-after": "60",
+      "Content-Type": "application/json"
+    };
+    axiosMock.onPost(requestParams.url).reply(200, {}, responseHeaders);
+    const restClient = new AxiosRestClient(IModelsErrorParser.parse);
+
+    // Act
+    const response = await restClient.sendPostRequest(requestParams);
+    const actualHeaders = response.headers.getAll();
+
+    // Assert
+    expect(JSON.stringify(actualHeaders)).to.deep.equal(JSON.stringify(expectedHeaders));
   });
 });
