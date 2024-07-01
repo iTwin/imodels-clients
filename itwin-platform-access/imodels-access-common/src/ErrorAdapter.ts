1dcee6b77f8b7afd4b1022512c4c19eee25fd7ab
Austeja Kalpakovaite <austeja.kalpakovaite@bentley.com>
2024-07-01 16:36:25 +0300

    Remove comment

--- itwin-platform-access/imodels-access-common/src/ErrorAdapter.ts
+++ itwin-platform-access/imodels-access-common/src/ErrorAdapter.ts
@@ -19,7 +19,7 @@ export class ErrorAdapter {
     if (!isIModelsApiError(error))
       return error;
 
-    if (error.code === IModelsErrorCode.Unrecognized) // TODO: extend with known error codes
+    if (error.code === IModelsErrorCode.Unrecognized)
       return error;
 
     if (ErrorAdapter.isAPIAuthError(error.code))
