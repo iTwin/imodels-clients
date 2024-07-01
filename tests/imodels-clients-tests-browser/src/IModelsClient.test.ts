dc83483de740deebd2470a4dca7053ef2e5d0647
Austeja Kalpakovaite <austeja.kalpakovaite@bentley.com>
2024-07-01 16:09:01 +0300

    Add browser integration tests

---
+++ tests/imodels-clients-tests-browser/src/IModelsClient.test.ts
@@ -0,0 +1,44 @@
+/*---------------------------------------------------------------------------------------------
+ * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
+ * See LICENSE.md in the project root for license terms and full copyright notice.
+ *--------------------------------------------------------------------------------------------*/
+
+import { IModelsClient } from "@itwin/imodels-client-management/lib/esm/IModelsClient";
+import { IModelsErrorCode } from "@itwin/imodels-client-management/lib/esm/base/types";
+import { assertError } from "@itwin/imodels-client-test-utils/lib/assertions/BrowserFriendlyAssertions";
+
+describe(`[Management] ${IModelsClient.name}`, () => {
+  it("should return original error information in case of network issue", function(done) {
+    // Arrange
+    const iModelsClient = new IModelsClient({
+      api: { baseUrl: "http://foo.bar" }
+    });
+
+    // Act
+    iModelsClient.iModels.getSingle({
+      authorization: async () => ({ scheme: "", token: "" }),
+      iModelId: "foo"
+    }).catch(err => {
+
+      // Assert
+      assertError({
+        objectThrown: err,
+        expectedError: {
+          code: IModelsErrorCode.Unrecognized,
+          message: "Unknown error occurred.\n"
+            + "Original error message: Network Error,\n"
+            + "original error code: ERR_NETWORK,\n"
+            + "response status code: undefined,\n"
+            + "response body: undefined",
+          originalError: {
+            code: "ERR_NETWORK",
+            name: "Error",
+            message: "Network Error"
+          }
+        }
+      });
+
+      done();
+    });
+  });
+});
--- tests/imodels-clients-tests/src/integration/management/IModelsClient.test.ts
+++ tests/imodels-clients-tests/src/integration/management/IModelsClient.test.ts
@@ -3,24 +3,14 @@
  * See LICENSE.md in the project root for license terms and full copyright notice.
  *--------------------------------------------------------------------------------------------*/
 
-import { IModelsClient, IModelsClientOptions, IModelsErrorCode } from "@itwin/imodels-client-management";
-import { TestUtilTypes, assertError } from "@itwin/imodels-client-test-utils";
+import { IModelsClient, IModelsErrorCode } from "@itwin/imodels-client-management";
+import {  assertError } from "@itwin/imodels-client-test-utils";
 
-import { getTestDIContainer } from "../common";
 
 describe("[Management] IModelsClient", () => {
-  let iModelsClientOptions: IModelsClientOptions;
-
-  before(async () => {
-    const container = getTestDIContainer();
-
-    iModelsClientOptions = container.get<IModelsClientOptions>(TestUtilTypes.IModelsClientOptions);
-  });
-
   it("should return original error information in case of network issue", async () => {
     // Arrange
     const iModelsClient = new IModelsClient({
-      ...iModelsClientOptions,
       api: { baseUrl: "http://foo.bar" }
     });
 
@@ -52,7 +42,5 @@ describe("[Management] IModelsClient", () => {
         }
       }
     });
-
   });
-
 });
