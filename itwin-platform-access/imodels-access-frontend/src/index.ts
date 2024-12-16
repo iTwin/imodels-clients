6cc4cb8f3aec0cad61fbbdbe01286009c35e1caf
Linas B <linas.burneika@bentley.com>
2024-12-16 12:40:13 +0200

    Adding ESM output for @itwin/imodels-access-common

--- itwin-platform-access/imodels-access-frontend/src/index.ts
+++ itwin-platform-access/imodels-access-frontend/src/index.ts
@@ -2,5 +2,5 @@
  * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
  * See LICENSE.md in the project root for license terms and full copyright notice.
  *--------------------------------------------------------------------------------------------*/
-export * from "@itwin/imodels-access-common/lib/AccessTokenAdapter";
+export * from "@itwin/imodels-access-common/lib/esm/AccessTokenAdapter";
 export * from "./FrontendIModelsAccess";
