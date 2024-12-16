c0ea109c40880cd9e2fa568367fd0bfb77725432
Linas B <linas.burneika@bentley.com>
2024-12-16 13:22:46 +0200

    Changed @itwin/imodels-access-common to have barrel export

---
+++ itwin-platform-access/imodels-access-common/src/index.ts
@@ -0,0 +1,5 @@
+export * from "./AccessTokenAdapter";
+export * from "./ChangesetFunctions";
+export * from "./Constants";
+export * from "./ErrorAdapter";
+export * from "./ErrorHandlingFunctions";
--- itwin-platform-access/imodels-access-frontend/src/index.ts
+++ itwin-platform-access/imodels-access-frontend/src/index.ts
@@ -2,5 +2,7 @@
  * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
  * See LICENSE.md in the project root for license terms and full copyright notice.
  *--------------------------------------------------------------------------------------------*/
-export * from "@itwin/imodels-access-common/lib/esm/AccessTokenAdapter";
+import { AccessTokenAdapter } from "@itwin/imodels-access-common";
+
 export * from "./FrontendIModelsAccess";
+export { AccessTokenAdapter };
