558c5801f677fdf7fcd14b89d2c1570fda8da896
Karolis Dziedzelis <3297568+Animagne@users.noreply.github.com>
2025-04-28 18:17:56 +0900

    Tests fix

--- utils/imodels-client-test-utils/src/index.ts
+++ utils/imodels-client-test-utils/src/index.ts
@@ -2,6 +2,8 @@
  * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
  * See LICENSE.md in the project root for license terms and full copyright notice.
  *--------------------------------------------------------------------------------------------*/
+import "reflect-metadata";
+
 export * from "./test-context-providers";
 export * from "./test-imodel-group/TestIModelGroup";
 export * from "./test-imodel-group/TestIModelGroupFactory";
