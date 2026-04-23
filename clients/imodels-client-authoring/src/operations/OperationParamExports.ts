139848d303986ea9410095a40fb64915959141a5
Aerilyn Dziedzelė <3297568+aerimagne@users.noreply.github.com>
2026-04-23 10:28:16 +0900

    Cleanup change files

--- clients/imodels-client-authoring/src/operations/OperationParamExports.ts
+++ clients/imodels-client-authoring/src/operations/OperationParamExports.ts
@@ -10,11 +10,11 @@ export * from "./changeset-extended-data/ChangesetExtendedDataOperationsParams";
 export * from "./changeset-group/ChangesetGroupOperationParams";
 export * from "./lock/LockOperationParams";
 
-/** @deprecated Import {@link GetLockListParams} directly from `@itwin/imodels-client-management` instead. */
+/** @deprecated in 6.2. Import {@link GetLockListParams} directly from `@itwin/imodels-client-management` instead. */
 export type { GetLockListParams } from "@itwin/imodels-client-management";
-/** @deprecated Import {@link GetLockListUrlParams} directly from `@itwin/imodels-client-management` instead. */
+/** @deprecated in 6.2. Import {@link GetLockListUrlParams} directly from `@itwin/imodels-client-management` instead. */
 export type { GetLockListUrlParams } from "@itwin/imodels-client-management";
-/** @deprecated Import {@link LockLevelFilter} directly from `@itwin/imodels-client-management` instead. */
+/** @deprecated in 6.2. Import {@link LockLevelFilter} directly from `@itwin/imodels-client-management` instead. */
 export type { LockLevelFilter } from "@itwin/imodels-client-management";
-/** @deprecated Import {@link ReleaseBriefcaseParams} directly from `@itwin/imodels-client-management` instead. */
+/** @deprecated in 6.2. Import {@link ReleaseBriefcaseParams} directly from `@itwin/imodels-client-management` instead. */
 export type { ReleaseBriefcaseParams } from "@itwin/imodels-client-management";
