cb23fefe725a79d92dcc74fe32a801088ba29ba3
Guillaume Valente <Guillaume.Valente@bentley.com>
2026-08-10 10:46:21 +0200

    move jsdoc

--- clients/imodels-client-management/src/operations/lock/LockOperations.ts
+++ clients/imodels-client-management/src/operations/lock/LockOperations.ts
@@ -49,13 +49,6 @@ export class LockOperations<
   }
 
   /**
-   * Releases Locks chunk for a specific Briefcase. This operation is used to release or abandon existing Locks.
-   * Wraps the {@link https://developer.bentley.com/apis/imodels-v2/operations/release-imodel-locks-chunk/
-   * Release iModel Locks Chunk} operation from iModels API.
-   * @param {ReleaseLocksChunkParams} params parameters for this operation. See {@link ReleaseLocksChunkParams}.
-   * @returns {Promise<ReleaseLocksChunkResult>} result indicating if this was the last chunk. See {@link ReleaseLocksChunkResult}.
-   */
-  /**
    * Gets Released Locks for a specific iModel. This method returns Released Locks in their full representation. The
    * returned iterator internally queries entities in pages. Wraps the
    * {@link https://developer.bentley.com/apis/imodels-v2/operations/get-imodel-released-locks/ Get iModel Released Locks}
@@ -79,6 +72,13 @@ export class LockOperations<
     );
   }
 
+  /**
+   * Releases Locks chunk for a specific Briefcase. This operation is used to release or abandon existing Locks.
+   * Wraps the {@link https://developer.bentley.com/apis/imodels-v2/operations/release-imodel-locks-chunk/
+   * Release iModel Locks Chunk} operation from iModels API.
+   * @param {ReleaseLocksChunkParams} params parameters for this operation. See {@link ReleaseLocksChunkParams}.
+   * @returns {Promise<ReleaseLocksChunkResult>} result indicating if this was the last chunk. See {@link ReleaseLocksChunkResult}.
+   */
   public async releaseLocksChunk(
     params: ReleaseLocksChunkParams
   ): Promise<ReleaseLocksChunkResult> {
